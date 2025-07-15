d983a2176e16be0138e759fdef93cdd42c6e2fdf
frectonz <fraol0912@gmail.com>
2025-07-15 15:50:02 +0300

    feat: setup better auth

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,6 +1,1 @@
-import { sqliteTable, integer } from 'drizzle-orm/sqlite-core';
-
-export const user = sqliteTable('user', {
-	id: integer('id').primaryKey(),
-	age: integer('age')
-});
+export * from './auth-schema';
